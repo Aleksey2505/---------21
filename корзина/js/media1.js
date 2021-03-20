3b08562b2a55cfafcdf2dc4bac2f6d34d51739d1
Aleksey2505 <ak2205781@gmail.com>
2021-03-20 18:45:13 +0800

    Add files via upload

---
+++ корзина/js/media1.js
@@ -0,0 +1,21 @@
+function foo() {
+   if (window.innerWidth < 1024) { 
+       
+       
+       
+       
+               /* все, что вы хотите здесь сделать */ 
+     }
+}
+
+window.addEventListener('changesize', foo);
+
+
+
+
+
+
+/*
+
+
+if (screen.width < 700) document.write ('<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></sc' + 'ript>');
